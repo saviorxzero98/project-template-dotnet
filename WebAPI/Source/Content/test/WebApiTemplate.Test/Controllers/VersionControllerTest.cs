adc86e5d2358c12bb3867eb5ae10409256bcf780
saviorxzero98 <saviorxzero98@gmail.com>
2024-04-25 18:02:08 +0800

    add: Test Project

---
+++ WebAPI/Source/Content/test/WebApiTemplate.Test/Controllers/VersionControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApiTemplate.Test.Controllers
+{
+    public class VersionControllerTest : IClassFixture<TestHostBaseFixture>
+    {
+        private readonly ServiceProvider _serviceProvider;
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        public VersionControllerTest(TestHostBaseFixture fixture)
+        {
+            _serviceProvider = fixture.ServiceProvider;
+            _server = fixture.Server;
+            _client = fixture.Client;
+        }
+
+        [Fact]
+        public Task Test1Async()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ WebAPI/Source/Content/test/WebApiTemplate.Test/TestHostBaseFixture.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApiTemplate.Test
+{
+    public class TestHostBaseFixture
+    {
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public TestServer Server { get; private set; }
+
+        public HttpClient Client { get; private set; }
+
+        public TestHostBaseFixture()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            // 讀取 appsettings.json
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json",
+                                                                       optional: true,
+                                                                       reloadOnChange: true)
+                                                          .Build();
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+
+            // 設定 Web Host，執行環境、Configutation、Web API 的 Startup 類別
+            var builder = new WebHostBuilder().UseEnvironment("yest")
+                                              .UseConfiguration(configuration)
+                                              .UseStartup<Startup>();
+
+            // 建立 Test Server 和 Http Client
+            Server = new TestServer(builder);
+            Client = Server.CreateClient();
+
+            // 設定 Service Provider
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+        }
+    }
+}
