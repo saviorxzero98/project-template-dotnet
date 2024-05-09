65763e1c687977e5262fa6c52878fbe532d74366
saviorxzero98 <saviorxzero98@gmail.com>
2024-05-09 13:49:30 +0800

    update: CommonEx.Utilities

---
+++ WebAPI/Source/Content/src/Common/CommonEx.Utilities/DateTimeUtilities/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace CommonEx.Utilities.DateTimeUtilities.Extensions
+{
+    public static class TimeSpanExtensions
+    {
+        private const double DaysOfYear = 365;
+        private const double MonthsOfYear = 12;
+        private const double DaysOfWeek = 7;
+        private const double DaysOfMonth = DaysOfYear / MonthsOfYear;
+
+        /// <summary>
+        /// 多少年
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static double GetTotalYears(this TimeSpan time)
+        {
+            return time.TotalDays / DaysOfYear;
+        }
+
+        /// <summary>
+        /// 多少個月
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static double GetTotalMonths(this TimeSpan time)
+        {
+            return time.TotalDays / DaysOfMonth;
+        }
+
+        /// <summary>
+        /// 多少週
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static double GetTotleWeeks(this TimeSpan time)
+        {
+            return time.TotalDays / DaysOfWeek;
+        }
+    }
+}
